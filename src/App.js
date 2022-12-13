218758c69958cc8cb1022d7375b0bcacd825c9e1
prakash chanchal <prakashchanchal@prakashs-MacBook-Pro.local>
2022-12-13 22:05:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 
 
-import React,{useState,Suspense,lazy,useEffect} from 'react';
 import Reactga from 'react-ga';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Routes,Route} from 'react-router-dom';
-const MovingHomepage=lazy(()=>import('./components/pages/Services/ServiceHomePage/MovingHomepage'));
-
+import MovingHomepage from './components/pages/Services/ServiceHomePage/MovingHomepage';
 function App() {
-  const [heightProfile] = useState('100vh');
-  useEffect(()=>{
-  Reactga.initialize('UA-235709629-1')
-  Reactga.pageview(window.location.pathname + window.location.search)
-  },[])
 
 
   const loading = () => (
@@ -20,7 +13,7 @@ function App() {
 
   return (
 
-    <div className="App" style={{ height: heightProfile }}>
+    <div className="App" style={{ height: '100vh' }}>
           <script>
 {  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="X54UDW2g0lhnWcXvCHFHXIcqZCCsYIyo";;analytics.SNIPPET_VERSION="4.15.3";
   analytics.load("X54UDW2g0lhnWcXvCHFHXIcqZCCsYIyo");
@@ -39,12 +32,11 @@ function App() {
 </script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10955203557"></script>
 
-        <Suspense fallback={loading()}
-      >
+   
               <Routes>
           <Route path="/" element={<MovingHomepage />} />
           </Routes>
-            </Suspense>
+           
 
     </div>
   );
